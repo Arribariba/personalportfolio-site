d8fc6bf4f65eccc210b70d4c89020a4db151507f
Mustafa Ocal <ocalmustafaa@gmail.com>
2026-02-20 18:45:36 +0300

    Update Navbar links and text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,9 +19,7 @@ export default function Navbar() {
 </Link>
           </div>
           <div className="hidden sm:flex space-x-8">
-            <Link href="/portfolio" className="text-stone-600 hover:text-stone-900 transition-colors font-medium">
-              Calismalar
-            </Link>
+            <Link href="/portfolio">Projeler</Link>
             <Link href="/blog" className="text-stone-600 hover:text-stone-900 transition-colors font-medium">
               Blog
             </Link>
